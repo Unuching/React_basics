4606cf927e0b87388bd855e0d4879a6d0d2e642e
Unuching Marma <unuching.marma2023@gmail.com>
2025-02-24 14:30:46 +0600

    new react app

---
+++ travel_journal/src/App.jsx
@@ -0,0 +1,5 @@
+import Header from "./my_components/header.jsx";
+
+export default function App() {
+  return <Header />;
+}
---
+++ travel_journal/src/my_components/header.jsx
@@ -0,0 +1,10 @@
+export default function Header() {
+  return (
+    <>
+      <header className="my_header">
+        <img src="/src/assets/globe.png" alt="" />
+        <h1>My Travel Journal</h1>
+      </header>
+    </>
+  );
+}
