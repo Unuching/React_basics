c8991ac86d4a4ccd74ec2d7a7bb174045aefbddc
Unuching Marma <unuching.marma2023@gmail.com>
2025-02-26 10:24:47 +0600

    add new code

--- travel_journal/src/my_components/entry.jsx
+++ travel_journal/src/my_components/entry.jsx
@@ -16,7 +16,7 @@ export default function Entry() {
           View on Google Maps
         </a>
         <h2 className="location">Mount Fuji</h2>
-        <p>12 Jan, 2021 - 24 Jan, 2021</p>
+        <p className="my_dates">12 Jan, 2021 - 24 Jan, 2021</p>
         <p>
           Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters
           (12,380 feet). Mount Fuji is the single most popular tourist site in
