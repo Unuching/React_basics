a1f79e8ed47c9f52c16a5392ca9f0d2531b8c343
Unuching Marma <unuching.marma2023@gmail.com>
2025-07-16 11:58:00 +0600

    add new file

---
+++ js_for_react/chaining.js
@@ -0,0 +1,5 @@
+const user = {
+  name: "Unu",
+};
+
+console.log(user.names ?? "Unknown");
---
+++ js_for_react/immutable.js
@@ -0,0 +1,8 @@
+//example of spread operator for immutability
+const myLists = ["Unu", "Mema"];
+
+const newlists = [...myLists, "Chengchengnu"];
+const anotherlists = [...newlists, "machingnu"];
+console.log(myLists);
+console.log(anotherlists);
+console.log(newlists);
