fa44ecb13224bb4f9ce5b615c256dad28425d5dc
Unuching Marma <unuching.marma2023@gmail.com>
2025-07-16 07:48:33 +0600

    add new files

---
+++ js_for_react/destructuring.js
@@ -0,0 +1,9 @@
+const peoples = [
+  { id: "a1", description: "A school teacher" },
+  { id: "a2", description: "A bank worker" },
+  { id: "b1", description: "A clerk" },
+  { id: "b2", description: "A office helper" },
+];
+
+const [a, b] = peoples;
+console.log(b);
