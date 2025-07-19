f5be8338ae866eb354f620b061dd87689e0d8469
Unuching Marma <unuching.marma2023@gmail.com>
2025-07-19 09:48:40 +0600

    update codebase

--- mini_project1/rating-UI/src/components/rating.jsx
+++ mini_project1/rating-UI/src/components/rating.jsx
@@ -1,15 +1,29 @@
+import { useState } from 'react';
 const Rating = () => {
+  const [rating, setRating] = useState(0);
+  const [hover, setHover] = useState(0);
   const stars = Array.from({ length: 5 }, (_, i) => i + 1);
+  const feedBackMessages = ['Terrible', 'Poor', 'Fair', 'Good', 'Excellent'];
+
   return (
-    <div className="rating-container">
+    <div className='rating-container'>
       <h2>Rate Your Experience</h2>
-      <div className="stars">
+      <div className='stars'>
         {stars.map((star) => (
-          <span key={star} className="star">
-            {"\u2605"}
+          <span
+            onClick={() => setRating(star)}
+            onMouseEnter={() => setHover(star)}
+            onMouseLeave={() => setHover(0)}
+            key={star}
+            className={`star ${star <= (hover || rating) ? 'active' : ''}`}
+          >
+            {'\u2605'}
           </span>
         ))}
       </div>
+      {rating > 0 && (
+        <p className='feedback'>{feedBackMessages[rating - 1]} </p>
+      )}
     </div>
   );
 };
