6fcc27dd7739186d11a88aef23a4ca4da81e1e20
Unuching Marma <unuching.marma2023@gmail.com>
2025-07-23 20:15:29 +0600

    update file

--- mini_project_2/notes-app/src/components/Notelist.jsx
+++ mini_project_2/notes-app/src/components/Notelist.jsx
@@ -1,4 +1,4 @@
-const NoteList = ({ notes }) => {
+const NoteList = ({ notes, deleteNote }) => {
   if (notes.length === 0) {
     return <p className='text-center text-gray-500'>No notes yet</p>;
   }
@@ -19,6 +19,12 @@ const NoteList = ({ notes }) => {
             {note.priority}
           </p>
           <p className='mt-2'>{note.description}</p>
+          <button
+            onClick={() => deleteNote(note.id)}
+            className='mt-3 text-lg font-bold text-red-500 cursor-pointer transition hover:text-red-700  '
+          >
+            🚮 Delete
+          </button>
         </div>
       ))}
     </div>
