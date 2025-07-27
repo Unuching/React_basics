76a77c4303da4fd609009828409666a134a5bcf7
Unuching Marma <unuching.marma2023@gmail.com>
2025-07-27 19:10:33 +0600

    add new file

---
+++ Javascript for react/arrow_functions.js
@@ -0,0 +1,11 @@
+// Arrow function let us write function in shorter form
+let multiply = (a, b) => a * b; // if a function have only one statement an return it, we don't have to use 'return' value
+
+//console.log(multiply(4, 5));
+
+// using parameters
+
+let hello = (val1, val2, val3) => 'hello ' + val1 + val2 + val3;
+// if you have one parameter you cal also skip the parenthesis
+
+console.log(hello('Unuching', 'how Are you', 'hmm, good'));
