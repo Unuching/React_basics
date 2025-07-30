29ce84bd0ed634fa2200dd1da8bf5401de246660
Unuching Marma <unuching.marma2023@gmail.com>
2025-07-30 20:18:36 +0600

    update codebase

--- NoteApp/src/App.jsx
+++ NoteApp/src/App.jsx
@@ -1,5 +1,9 @@
 const APP = () => {
-  return <div className="">'Hello World'</div>;
+  return (
+    <div className='max-w-lg mx-auto mt-10 p-6 bg-gray-100 rounded-lg shadow-lg '>
+      <h2 className='text-2xl font-bold text-center mb-4'> 📝 Notes App</h2>
+    </div>
+  );
 };
 
 export default APP;
