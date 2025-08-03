a4c86da37ddf00bf86bb87209ca58cfd745eb4b7
Unuching Marma <unuching.marma2023@gmail.com>
2025-08-03 22:18:18 +0600

    update codebase

--- NoteApp/src/components/noteList.jsx
+++ NoteApp/src/components/noteList.jsx
@@ -1,5 +1,4 @@
-
-
+import Note from './note';
 const NoteList = ({ notes, deleteNote }) => {
   console.log(notes);
 
@@ -9,30 +8,7 @@ const NoteList = ({ notes, deleteNote }) => {
   return (
     <div className='space-y-4'>
       {notes.map((note) => (
-        <div
-          key={note.id}
-          className='p-4 bg-white rounded-lg shadow-md border-l-4'
-        >
-          <h3 className='text-lg font-bold'>{note.title}</h3>
-          <p className='text-sm text-gray-600'>
-            <strong>Category: </strong>
-            {note.category}
-          </p>
-          <p className='text-sm text-gray-600'>
-            <strong>Priority: </strong>
-            {note.priority}
-          </p>
-          <p className='mt-2'>
-            <strong>Description: </strong>
-            {note.description}
-          </p>
-          <button
-            onClick={()=>deleteNote(note.id)}
-            className='mt-3 text-red-500 cursor-pointer transition hover:text-red-700'
-          >
-            🗑️ Delete
-          </button>
-        </div>
+        <Note key={note.id} note={note} deleteNote={deleteNote} />
       ))}
     </div>
   );
