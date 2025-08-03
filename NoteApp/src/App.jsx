fd8bd98996ca89029dd47831bb08946895ccb13d
Unuching Marma <unuching.marma2023@gmail.com>
2025-08-03 21:35:43 +0600

    update codebase

--- NoteApp/src/App.jsx
+++ NoteApp/src/App.jsx
@@ -5,12 +5,19 @@ import NoteList from './components/noteList';
 const APP = () => {
   const [notes, setNotes] = useState([]);
 
+  const deleteNote = (id) => {
+    const confirmDelete = window.confirm('Are You Sure?');
+    if (confirmDelete) {
+      setNotes(notes.filter((note) => note.id != id));
+    }
+  };
+
   return (
     <div className='max-w-lg mx-auto mt-10 p-6 bg-gray-100 rounded-lg shadow-lg '>
       <h2 className='text-2xl font-bold text-center mb-4'> 📝 Notes App</h2>
 
       <NoteForm notes={notes} setNotes={setNotes} />
-      <NoteList notes={notes} />
+      <NoteList notes={notes} deleteNote={deleteNote} />
     </div>
   );
 };
--- NoteApp/src/components/noteList.jsx
+++ NoteApp/src/components/noteList.jsx
@@ -1,24 +1,6 @@
-// const NoteList = ({ notes }) => {
-//   if (notes.length === 0) {
-//     return <p className='text-center text-gray-500'>No Notes Yet</p>;
-//   }
-//   return (
-//     <div className='space-y-4'>
-//       {notes.map((note) => (
-//         <div
-//           key={note.id}
-//           className='p-4 bg-white rounded-lg shadow-md border-l-4'
-//         >
-//           <h3 className='text-lg font-bold'>{note.title}</h3>
-//         </div>
-//       ))}
-//     </div>
-//   );
-// };
 
-// export default NoteList;
 
-const NoteList = ({ notes }) => {
+const NoteList = ({ notes, deleteNote }) => {
   console.log(notes);
 
   if (notes.length === 0) {
@@ -44,6 +26,12 @@ const NoteList = ({ notes }) => {
             <strong>Description: </strong>
             {note.description}
           </p>
+          <button
+            onClick={()=>deleteNote(note.id)}
+            className='mt-3 text-red-500 cursor-pointer transition hover:text-red-700'
+          >
+            🗑️ Delete
+          </button>
         </div>
       ))}
     </div>
