02049616c5baa543cff7a7ecbc9a273268fe4875
Unuching Marma <unuching.marma2023@gmail.com>
2025-08-04 22:16:51 +0600

    update codebase

--- timerApp/src/App.jsx
+++ timerApp/src/App.jsx
@@ -1,5 +1,23 @@
+import { useState, useRef } from 'react';
+
 const App = () => {
-  return <h1>My App</h1>;
+  const timerRef = useRef(null);
+  const [time, setTime] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+
+  const toggleTimer = ()=>{
+    
+  }
+
+  return (
+    <div className='max-w-md mx-auto mt-10 p-6 bg-gray-100 rounded-lg shadow-lg text-center'>
+      <h2 className='text-4xl font-semibold mt-4'>⏰ Timer: {time}</h2>
+
+      <button onClick={toggleTimer} className='mt-3 bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600'>
+        {isRunning ? 'Pause' : 'Start'}
+      </button>
+    </div>
+  );
 };
 
 export default App;
