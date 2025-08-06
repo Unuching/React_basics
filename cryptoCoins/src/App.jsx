0e65c355e9d8ddf748df09d6934c1c1f93f9a689
Unuching Marma <unuching.marma2023@gmail.com>
2025-08-06 12:40:54 +0600

    add new code

--- cryptoCoins/src/App.jsx
+++ cryptoCoins/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 
 const API_URL =
-  'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&sparkline=false';
+  'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false';
 
 const App = () => {
   const [coins, setCoins] = useState([]);
@@ -9,20 +9,35 @@ const App = () => {
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    fetch(API_URL)
-      .then((res) => {
-        if (!res.ok) throw new Error('Failed to get data');
-        return res.json();
-      })
-      .then((data) => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch(API_URL);
+        if (!res.ok) throw new Error('Failed to get Data');
+        const data = await res.json();
         console.log(data);
         setCoins(data);
-        setLoading(false);
-      })
-      .catch((err) => {
+      } catch (err) {
         setError(err.message);
+      } finally {
         setLoading(false);
-      });
+      }
+    };
+    fetchData();
+
+    // fetch(API_URL)
+    //   .then((res) => {
+    //     if (!res.ok) throw new Error('Failed to get data');
+    //     return res.json();
+    //   })
+    //   .then((data) => {
+    //     console.log(data);
+    //     setCoins(data);
+    //     setLoading(false);
+    //   })
+    //   .catch((err) => {
+    //     setError(err.message);
+    //     setLoading(false);
+    //   });
   }, []);
 
   return (
