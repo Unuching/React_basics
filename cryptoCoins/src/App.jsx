4ba67f544d5d11613dfe5a09d168933352feb5fb
Unuching Marma <unuching.marma2023@gmail.com>
2025-08-07 09:58:15 +0600

    update file

--- cryptoCoins/src/App.jsx
+++ cryptoCoins/src/App.jsx
@@ -43,6 +43,35 @@ const App = () => {
   return (
     <div>
       <h1> 🚀 Hello Crypto</h1>
+      {loading && <p>Loading....</p>}
+      {error && <div className='error'> {error}</div>}
+
+      {!loading && !error && (
+        <main className='grid'>
+          {coins.map((coin) => (
+            <div key={coin.id} className='coin-card '>
+              <div className='coin-header'>
+                <img src={coin.image} alt={coin.name} className='coin-image' />
+                <div>
+                  <h2>{coin.name}</h2>
+                  <p className='symbol'>{coin.symbol.toUpperCase()}</p>
+                </div>
+              </div>
+              <p>Price: ${coin.current_price.toLocaleString()}</p>
+              <p
+                className={
+                  coin.price_change_percentage_24h >= 0
+                    ? 'positive'
+                    : 'negative'
+                }
+              >
+                {coin.price_change_percentage_24h.toFixed(2)}
+              </p>
+              <p>Market Cap: ${coin.market_cap.toLocaleString()}</p>
+            </div>
+          ))}
+        </main>
+      )}
     </div>
   );
 };
