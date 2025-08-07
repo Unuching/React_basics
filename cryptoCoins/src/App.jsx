7d5d342124b22fc2af8230e3db7834aee42f526f
Unuching Marma <unuching.marma2023@gmail.com>
2025-08-07 10:03:04 +0600

    update file

--- cryptoCoins/src/App.jsx
+++ cryptoCoins/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import CoinCard from './components/coinCard';
 
 const API_URL =
   'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false';
@@ -49,26 +50,7 @@ const App = () => {
       {!loading && !error && (
         <main className='grid'>
           {coins.map((coin) => (
-            <div key={coin.id} className='coin-card '>
-              <div className='coin-header'>
-                <img src={coin.image} alt={coin.name} className='coin-image' />
-                <div>
-                  <h2>{coin.name}</h2>
-                  <p className='symbol'>{coin.symbol.toUpperCase()}</p>
-                </div>
-              </div>
-              <p>Price: ${coin.current_price.toLocaleString()}</p>
-              <p
-                className={
-                  coin.price_change_percentage_24h >= 0
-                    ? 'positive'
-                    : 'negative'
-                }
-              >
-                {coin.price_change_percentage_24h.toFixed(2)}
-              </p>
-              <p>Market Cap: ${coin.market_cap.toLocaleString()}</p>
-            </div>
+            <CoinCard key={coin.id} coin={coin} />
           ))}
         </main>
       )}
