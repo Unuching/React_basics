fe5498e7113805e078f575ed15f12b7479d34b54
Unuching Marma <unuching.marma2023@gmail.com>
2025-08-08 21:44:49 +0600

    Auto: 1 added - 2025-08-08

---
+++ JAvascript exercise/8th_August_2025.js
@@ -0,0 +1,25 @@
+const myNumbers = [1, 2, 3, 4, 5];
+
+let labeledNumbers = myNumbers.map((num) => {
+  return `Number: ${num}`;
+});
+//console.log(labeledNumbers);
+
+const myNumberArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+let labelNums = myNumberArray
+  .filter((even) => {
+    return even % 2 === 0;
+  })
+  .map((newNum) => {
+    return `Number:${newNum}`;
+  });
+//console.log(labelNums);
+
+
+const users = [
+  { name: 'Alice', age: 25 },
+  { name: 'Bob', age: 17 },
+  { name: 'Charlie', age: 30 },
+  { name: 'David', age: 15 },
+];
