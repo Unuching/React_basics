0715b7c4cebb393cb9f2afae92cede5ce736a69e
Unuching Marma <unuching.marma2023@gmail.com>
2025-08-10 12:05:54 +0600

    add new files

--- cryptoCoins/src/App.jsx
+++ cryptoCoins/src/App.jsx
@@ -1,8 +1,6 @@
 import { useState, useEffect } from 'react';
-import CoinCard from './components/coinCard';
-import LimitSelector from './components/limitSelector';
-import FilterInput from './components/filterInput';
-import SortSelector from './components/sortSelector';
+import HomePage from './pages/home';
+import { Routes, Route } from 'react-router';
 
 const API_URL = import.meta.env.VITE_API_URL;
 
@@ -48,53 +46,26 @@ const App = () => {
     //   });
   }, [limit]);
 
-  const filteredCoins = coins
-    .filter((coin) => {
-      return (
-        coin.name.toLowerCase().includes(filter.toLowerCase()) ||
-        coin.symbol.toLowerCase().includes(filter.toLowerCase())
-      );
-    })
-    .slice()
-    .sort((a, b) => {
-      switch (sortBy) {
-        case 'market_cap_desc':
-          return b.market_cap - a.market_cap;
-        case 'market_cap_asc':
-          return a.market_cap - b.market_cap;
-        case 'price_desc':
-          return b.current_price - a.current_price;
-        case 'price_asc':
-          return a.current_price - b.current_price;
-        case 'change_desc':
-          return b.price_change_percentage_24h - a.price_change_percentage_24h;
-
-        case 'change_asc':
-          return a.price_change_percentage_24h - b.price_change_percentage_24h;
-      }
-    });
-
   return (
-    <div>
-      <h1> 🚀 Hello Crypto</h1>
-      {loading && <p>Loading....</p>}
-      {error && <div className='error'> {error}</div>}
-      <div className='top-controls'>
-        <FilterInput filter={filter} onFilterChange={setFilter} />
-        <LimitSelector limit={limit} onLimitChange={setLimit} />
-        <SortSelector sortBy={sortBy} onSortChange={setSortBy} />
-      </div>
-
-      {!loading && !error && (
-        <main className='grid'>
-          {filteredCoins.length > 0 ? (
-            filteredCoins.map((coin) => <CoinCard key={coin.id} coin={coin} />)
-          ) : (
-            <p>No Matching Coins</p>
-          )}
-        </main>
-      )}
-    </div>
+    <Routes>
+      <Route
+        path='/'
+        element={
+          <HomePage
+            coins={coins}
+            filter={filter}
+            setFilter={setFilter}
+            limit={limit}
+            setLimit={setLimit}
+            sortBy={sortBy}
+            setSortBy={setSortBy}
+            loading={loading}
+            error={error}
+          />
+        }
+      />
+      
+    </Routes>
   );
 };
 
