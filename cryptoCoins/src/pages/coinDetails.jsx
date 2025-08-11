7e357b7e7c8116479e4d059d6baa4c8847d1824e
Unuching Marma <unuching.marma2023@gmail.com>
2025-08-11 08:22:27 +0600

    update codebase

--- cryptoCoins/src/pages/coinDetails.jsx
+++ cryptoCoins/src/pages/coinDetails.jsx
@@ -1,8 +1,33 @@
-import { useParams } from 'react-router';
+import { data, useParams } from 'react-router';
 import { useState, useEffect } from 'react';
+
+const API_URL = import.meta.env.VITE_COIN_API_URL;
 
 const CoinDetailsPage = () => {
   const { id } = useParams();
+  const [coin, setCoin] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchCoin = async () => {
+      try {
+        const res = await fetch(`${API_URL}/${id}`);
+
+        if (!res.ok) throw new Error('Failed To get Data');
+
+        const Data = await res.json();
+        console.log(Data);
+        setCoin(Data);
+      } catch (err) {
+        console.log(err);
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchCoin();
+  }, [id]);
 
   return <div>Coin Details {id}</div>;
 };
