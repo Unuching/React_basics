880e9db1409aa6590d51aedbfef14d3dd1adfc32
Unuching Marma <unuching.marma2023@gmail.com>
2025-08-12 08:09:55 +0600

    add new code

--- cryptoCoins/src/components/coinChart.jsx
+++ cryptoCoins/src/components/coinChart.jsx
@@ -1,3 +1,28 @@
+import { useState, useEffect } from 'react';
+import { Line } from 'react-chartjs-2';
+import {
+  Chart as Chartjs,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Tooltip,
+  Legend,
+  TimeScale,
+} from 'chart.js';
+
+import 'chartjs-adapter-date-fns';
+
+Chartjs.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Tooltip,
+  Legend,
+  TimeScale
+);
+
 const API_URL = import.meta.env.VITE_COIN_API_URL;
 
 const CoinChart = ({ coinID }) => {
