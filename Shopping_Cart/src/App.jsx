d3cbf6d123c7008200d8446f925f356a0f3ee9d7
Unuching Marma <unuching.marma2023@gmail.com>
2025-08-12 21:34:13 +0600

    Auto: 2 modified - 2025-08-12

--- Shopping_Cart/src/App.jsx
+++ Shopping_Cart/src/App.jsx
@@ -1,7 +1,30 @@
+import { useState, useEffect } from 'react';
+
 const App = () => {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const res = await fetch('http://localhost:8000/products');
+        if (!res.ok) throw new Error('Failed to get products');
+        const data = await res.json();
+        console.log(data);
+        setProducts(data);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchProducts();
+  }, []);
+
   return (
-    <div>
-      <h1>Hello</h1>
+    <div className='min-h-screen bg-gray-100'>
+      <h1 className='text-3xl font-bold mb-6'>Products Catalog</h1>
     </div>
   );
 };
